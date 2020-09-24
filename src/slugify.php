231fa3c8398ca55bff93da8a768b22e18cef5f31
Luke Kozak <kozakluke@gmail.com>
2020-09-24 13:19:42 +0200

    Add slugify util

---
+++ src/slugify.php
@@ -0,0 +1,17 @@
+<?php
+namespace kozakl\utils;
+
+function slugify($text)
+{
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    $text = trim($text, '-');
+    // remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+    $text = strtolower($text);
+    
+    return $text;
+}
