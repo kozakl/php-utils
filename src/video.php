6989c68c221cb5d4b57e3b2549ba6f2a0608614a
Luke Kozak <kozakluke@gmail.com>
2020-10-07 15:17:14 +0200

    Fix whitespaces in paths in resizeVideo

--- src/video.php
+++ src/video.php
@@ -6,7 +6,7 @@ function resizeVideo($data, $log)
     $data->width = ($data->width / 2 | 0) * 2;
     $options = [
         "-ss $data->start",
-        "-i $data->src",
+        "-i '$data->src'",
         $data->duration ?
             "-t $data->duration" : '',
         "-crf $data->quality",
@@ -15,7 +15,7 @@ function resizeVideo($data, $log)
     $log->info('FFmpeg',
         (array)shell_exec("ffmpeg "
             .join(' ', $options) .
-            " -y -an -vcodec libx264 $data->dest 2>&1"));
+            " -y -an -vcodec libx264 '$data->dest' 2>&1"));
     if (!filesize("$data->dest")) {
         unlink("$data->dest");
     }
