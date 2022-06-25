39406983fdc85a73076ef71e58d6f8870194e42a
Luke Kozak <kozakluke@gmail.com>
2022-06-25 08:57:37 +0200

    Add throws decoration in uploadedImages

--- src/uploadedImages.php
+++ src/uploadedImages.php
@@ -1,6 +1,10 @@
 <?php
 namespace kozakl\utils;
+use Exception;
 
+/**
+ * @throws Exception
+ */
 function uploadedImages($uploaded, $body, $path) {
     foreach ($body ?? [] as $key => $val) {
         is_null($val) &&
