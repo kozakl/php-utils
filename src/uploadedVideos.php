8e994a2f87dcc547548a14c98811bc5e87021cd4
Luke Kozak <kozakluke@gmail.com>
2022-09-09 18:56:08 +0200

    Update uploadedVideo util

--- src/uploadedVideos.php
+++ src/uploadedVideos.php
@@ -7,8 +7,23 @@ use Exception;
  */
 function uploadedVideos($uploaded, $body, $path) {
     foreach ($body ?? [] as $key => $val) {
-        is_null($val) &&
-            $body[$key] = new UploadedVideo(array_shift($uploaded), $path);
+        if (isset($val['file'])) {
+            $video = array_shift($uploaded);
+            if (!$video) {
+                throw new Exception('Video cannot be null');
+            } else if ($video->getError() !== UPLOAD_ERR_OK) {
+                throw new Exception('Video cannot be uploaded');
+            }
+            if (!is_dir("../public/uploads/{$path}")) {
+                mkdir("../public/uploads/{$path}", 0777, true);
+            }
+            $uniqName = uniqid(). '__'.
+                $video->getClientFilename();
+            $video->moveTo("../public/uploads/{$path}/{$uniqName}");
+            
+            $body[$key]['url'] = "uploads/{$path}/". rawurlencode($uniqName);
+            unset($body[$key]['file']);
+        }
     }
     return $body;
 }
