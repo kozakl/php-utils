fa1fd561c98e23cf6b49143b25a9469e75267ae5
Luke Kozak <kozakluke@gmail.com>
2022-09-15 20:52:59 +0200

    Rewrite uploadedImages

--- src/uploadedImages.php
+++ src/uploadedImages.php
@@ -7,8 +7,23 @@ use Exception;
  */
 function uploadedImages($uploaded, $body, $path) {
     foreach ($body ?? [] as $key => $val) {
-        is_null($val) &&
-            $body[$key] = new UploadedImage(array_shift($uploaded), $path);
+        if (isset($val['file'])) {
+            $image = array_shift($uploaded);
+            if (!$image) {
+                throw new Exception('Image cannot be null');
+            } else if ($image->getError() !== UPLOAD_ERR_OK) {
+                throw new Exception('Image cannot be uploaded');
+            }
+            if (!is_dir("../public/uploads/{$path}")) {
+                mkdir("../public/uploads/{$path}", 0777, true);
+            }
+            $uniqName = uniqid(). '__'.
+                $image->getClientFilename();
+            $image->moveTo("../public/uploads/{$path}/{$uniqName}");
+            
+            $body[$key]['url'] = "uploads/{$path}/". rawurlencode($uniqName);
+            unset($body[$key]['file']);
+        }
     }
     return $body;
 }
