2f8dd3a27d99ee7d08e903c6603ee23662c9153c
Luke Kozak <kozakluke@gmail.com>
2023-02-05 10:38:14 +0100

    Fix isHoliday function in date util

--- src/date.php
+++ src/date.php
@@ -1,6 +1,18 @@
 <?php
 namespace kozakl\utils\date;
 
+function getHolidays($countryCode) {
+    $holidays = [
+        'PL' => [
+            '01-01', '01-06', '05-01',
+            '05-03', '08-15', '11-01',
+            '11-11', '12-25', '12-24',
+            '12-26'
+        ]
+    ];
+    return $holidays[$countryCode];
+}
+
 function isWeekend($date) {
     return date('N', strtotime($date)) >= 6;
 }
@@ -11,8 +23,8 @@ function isEaster($date, $corpusChristi = true) {
         ($corpusChristi && $date == date('Y-m-d', strtotime($easter. '+60day')));
 }
 
-function isHoliday($date) {
+function isHoliday($date, $countryCode = 'PL') {
     return isWeekend($date) ||
-           isEaster($date)  ||
-           in_array(date('m-d', strtotime($date)), self::$holidays);
+        isEaster($date) ||
+        in_array(date('m-d', strtotime($date)), getHolidays($countryCode));
 }
