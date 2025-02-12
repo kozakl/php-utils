1c01b47031f8872a450a0e5a23bd093ca3dd45dd
Luke Kozak <kozakluke@gmail.com>
2025-02-12 09:02:37 +0100

    Add executeQuery.php util

---
+++ src/executeQuery.php
@@ -0,0 +1,11 @@
+<?php
+function executeQuery($db, $query, $params, $paramTypes = []) {
+    $stmt = $db->prepare($query);
+    foreach ($params as $key => $value) {
+        $type = $paramTypes[$key] ?? PDO::PARAM_STR;
+        $stmt->bindValue($key, $value, $type);
+    }
+    
+    $stmt->execute();
+    return $stmt;
+}
