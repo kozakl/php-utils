ce571badd458f1c8510fa3568842463b39f630b8
Luke Kozak <kozakluke@gmail.com>
2025-02-22 17:02:07 +0100

    Add removePolishChars to string util

--- src/string.php
+++ src/string.php
@@ -22,3 +22,13 @@ function removeFrom($str, $search, $offsetRemove = 0, $offsetSearch = 0) {
 function startsWith($str, $search) {
   return strpos($str, $search) === 0;
 }
+
+function removePolishChars($str) {
+    $map = [
+        'ą' => 'a', 'ć' => 'c', 'ę' => 'e', 'ł' => 'l',
+        'ń' => 'n', 'ó' => 'o', 'ś' => 's', 'ź' => 'z', 'ż' => 'z',
+        'Ą' => 'A', 'Ć' => 'C', 'Ę' => 'E', 'Ł' => 'L',
+        'Ń' => 'N', 'Ó' => 'O', 'Ś' => 'S', 'Ź' => 'Z', 'Ż' => 'Z'
+    ];
+    return strtr($str, $map);
+}
